2246e5f0000315d2cb06f1380dcb2aaf47dd427f
Andrew Miotke <andrewmiotke@gmail.com>
2020-11-01 17:28:14 -0800

    Bootstrap: Updated and fixed the bootstrap script

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# This is a small and novice script that helps bootstrap the setup 
+# This is a small and novice script that helps bootstrap the setup
 # of a new Mac. Continue to add to it by creating other functions below.
 
 import os
@@ -9,19 +9,26 @@ import os
 command_line_installs = [
     # Install Homebrew
     '/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)\"',
+
     # Install vim
     'brew install vim',
+
     # Install DB Browser for SQLite
     'brew cask install db-browser-for-sqlite',
+
     # Install tree
     'brew install tree',
-    # Clone dotfiles repo
-    'https://github.com/miotke/dotfiles.git ~/dotfiles',
+
     # Install the GitHub CLI tools
-    'brew install gh'
+    'brew install gh',
+
+    # Install htop
+    'brew install htop',
+
     # Install vim-plug
     'curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
+
     # Install oh-my-zsh
     'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',
 ]
@@ -32,11 +39,13 @@ sym_link_files = ['vimrc', 'zshrc']
 
 def install_from_homebrew():
     for command in command_line_installs:
+        print(f'Installing {command}...')
         os.system(command)
 
 
 def create_directories():
-    os.system('mkdir ~/Desktop/stuff')
+    # Creats a directory for all Git repos that I work in.
+    os.system('mkdir ~/Git')
 
 
 def setup_commands():
@@ -45,11 +54,11 @@ def setup_commands():
     os.system(show_xcode_build_times)
 
 
-def create_sym_links(): 
+def create_sym_links():
     # Creates sym links for vimrc, zshrc, ect.
     for file in sym_link_files:
-         print('Creating sym links for {file}')
-         os.system(f'ln -s ~/dotfiles/{file} ~/.{file}')
+        print(f'Creating sym links for {file}')
+        os.system(f'ln -s ~/dotfiles/{file} ~/.{file}')
 
 
 if __name__ == '__main__':
