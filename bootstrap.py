efacb8413d7b3a6d9cde3196711d50da24d0b841
Andrew Miotke <andrewmiotke@gmail.com>
2021-09-01 16:29:08 -0700

    Updated the bootstrap script to include the setup_nvim.py script

--- bootstrap.py
+++ bootstrap.py
@@ -5,66 +5,63 @@
 
 import os
 
-# Installs Homebrew and various applications
-command_line_installs = [
+# Installs Homebrew and various packages
+INSTALL_PACKAGES = [
     # Install Homebrew
-    '/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)\"',
-
-    # Install vim
-    'brew install vim',
-
+    '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"',
     # Install MonitorControl
-    'brew install --cask monitorcontrol',
-
+    "brew install --cask monitorcontrol",
     # Install DB Browser for SQLite
-    'brew cask install db-browser-for-sqlite',
-
+    "brew cask install db-browser-for-sqlite",
     # Install tree
-    'brew install tree',
-
+    "brew install tree",
     # Install the GitHub CLI tools
-    'brew install gh',
-
+    "brew install gh",
     # Install htop
-    'brew install htop',
-
-    # Install vim-plug
-    'curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
-
+    "brew install htop",
+    # Install vim-plug for Neovim
+    "curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
     # Install Neovim
-    'brew install neovim'
-
+    "brew install neovim"
     # Install oh-my-zsh
     'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',
 ]
 
-
-sym_link_files = ['vimrc', 'zshrc']
+SYMLINK_FILES = ["vimrc", "zshrc"]
+HOME = os.environ["HOME"]
 
 
 def install_from_homebrew():
-    for command in command_line_installs:
-        print(f'Installing {command}')
-        os.system(command)
+    for command in INSTALL_PACKAGES:
+        try:
+            print(f"Installing {command}")
+            os.system(command)
+        except:
+            print(f"Failed to install {command}")
 
 
 def create_directories():
-    # Creats a directory for all Git repos that I work in.
-    os.system('mkdir ~/Developer')
+    # Creats a directory for all Git repos
+    os.system("mkdir ~/Developer")
 
 
 def setup_commands():
-    show_xcode_build_times = 'defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES'
-
+    # Enables build times in Xcode. This might not be needed in Xcode 13
+    show_xcode_build_times = ("defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES")
     os.system(show_xcode_build_times)
 
+    # Changes the default git editor to Neovim
+    os.system("git config --global core.editor nvim")
 
-def create_sym_links():
-    # Creates sym links for vimrc, zshrc, ect.
-    for file in sym_link_files:
-        print(f'Creating sym links for {file}')
-        os.system(f"ln -s ~/dotfiles/{file} ~/.{file}")
+
+def create_symlinks():
+    # Creates symlinks for vimrc and zshrc
+    for file in SYMLINK_FILES:
+        print(f"Creating sym links for {file}")
+        os.system(f"ln -s {HOME}/dotfiles/{file} ~/.{file}")
+
+    # Creates a symlink for the nvim folder which contains all Neovim config files
+    os.system(f"ln -s {HOME}/dotfiles/nvim {HOME}/.config")
 
 
 def vs_code_settings():
@@ -73,9 +70,9 @@ def vs_code_settings():
     os.system("ln -s ~/dotfiles/keybindings.json /Users/andrewmiotke/Library/Application\ Support/Code/User/keybindings.json")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     install_from_homebrew()
     create_directories()
     setup_commands()
-    create_sym_links()
+    create_symlinks()
 
