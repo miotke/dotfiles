0502ce0ab4ee8e85d6d330db81d64acca3f059c2
Andrew Miotke <andrewmiotke@gmail.com>
2021-09-01 15:04:55 -0700

    Finished script for setting up neovim

--- setup_neovim.py
+++ setup_neovim.py
@@ -6,9 +6,19 @@ import os
 def main():
     HOME = os.environ['HOME']
 
-    os.system(f"mkdir {HOME}/.config/nvim/")
-    os.system(f"mkdir {HOME}/.config/nvim/colors/")
+    # Symlink dotfiles/nvim to the .config folder.
+    os.system(f"ln -s {HOME}/dotfiles/nvim {HOME}/.config")
+
+    # Install vim-plug
+    os.system("curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim")
+
+    # Change the git text editor to neovim
+    os.system("git config --global core.editor nvim")
+
+    # Create an alias for neovim
+    os.system("alias vim=nvim")
 
 
 if __name__ == "__main__":
     main()
+
