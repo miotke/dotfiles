814e19771f438980c97314bf90a881c7fbd84aec
Andrew Miotke <andrewmiotke@gmail.com>
2021-09-30 11:23:11 -0700

    Update setup_neovim.py

--- setup_neovim.py
+++ setup_neovim.py
@@ -5,12 +5,12 @@ import os
 
 def main():
     HOME = os.environ['HOME']
+    
+    # Install vim-plug
+    os.system("curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim")
 
     # Symlink dotfiles/nvim to the .config folder.
     os.system(f"ln -s {HOME}/dotfiles/nvim {HOME}/.config")
-
-    # Install vim-plug
-    os.system("curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim")
 
     # Change the git text editor to neovim
     os.system("git config --global core.editor nvim")
