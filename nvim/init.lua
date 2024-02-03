d8f71eb6920f727fe3aa1a154941f6e1927b823b
Andrew Miotke <andrew@andrewmiotke.com>
2024-02-03 11:21:05 -0800

    Organized init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,14 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.keymap.set('', '<space><space>', '<nop>', { noremap = true, silent = true }) 
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ";"
+--vim.keymap.set('', '<space><space>', '<nop>', { noremap = true, silent = true }) 
 
 -- Requirements
 require ("user.launch")
 require ("user.options")
 require ("user.keymaps")
---require ("user.telescope")
-
-spec ("user.telescope")
 
 -- Specs (plugins)
+spec ("user.telescope")
 spec ("user.colorscheme")
 spec ("user.treesitter")
 spec ("user.mason")
