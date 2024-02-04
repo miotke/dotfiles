9d3cccb898fc2b607cfa3a8fe030d3888063ebd7
Andrew Miotke <andrew@andrewmiotke.com>
2024-02-03 16:59:00 -0800

    Indent-blankline config

--- nvim/lua/user/indent-blankline.lua
+++ nvim/lua/user/indent-blankline.lua
@@ -1,6 +1,19 @@
 local M = {
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
+    opts = { 
+	indent_blankline_use_treesitter = true,
+	show_current_context = true,
+	show_current_context_start = true,
+	indent_blankline_show_trailing_blankline_indent = true
+    }
 }
+
+function M.config()
+    require("ibl").setup { 
+	-- Settings go in opts above 
+    }
+end
 
 return M
 
