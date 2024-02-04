3c5b047ff36c31510ceae102ca35d7eab4647897
Andrew Miotke <andrew@andrewmiotke.com>
2024-02-03 18:08:31 -0800

    Telescope: Got telescope to behave and work with keybinds

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,32 +1,6 @@
 -- [[ Basic Keymaps ]]
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
-
--- TESTING FUNCTION WHICH TURNS ON/OFF RELATIVE NUMBERS
-function toggle_stuff()
-    -- sort of a "ternary" operator in lua
-    vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
-    vim.o.relativenumber = not vim.o.relativenumber
-end
-keymap('n', '<leader>s', ':lua toggle_stuff()<CR>', opts)
-
---local builtin = require("telescope.builtin")
---keymap("n", "<leader>ff", builtin.find_files, {})
-
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
---keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
--- keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
--- keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Diagnostic keymaps
--- keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
--- keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
--- keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
--- keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
  
 -- Keeps current line in the center when searching
 keymap("n", "n", "nzz")
@@ -35,15 +9,10 @@ keymap("n", "n", "nzz")
 keymap("v", "<", "<gv")
 keymap("v", ">", ">gv")
 
--- Telescope
---keymap('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
--- keymap('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
---keymap('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
---keymap('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles in current directory' })
---keymap('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
---keymap('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
---keymap('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
---keymap('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
---keymap('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
--- keymap('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+-- Toggle relativenumbers
+keymap("n", "<leader>n", function()
+   -- sort of a "ternary" operator in lua
+   vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
+   vim.o.relativenumber = not vim.o.relativenumber               
+end, { desc = "Toggles relativenumbers off and on" })
 
--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -15,17 +15,7 @@ local M = {
 
 function M.config()
   local wk = require "which-key"
-  wk.register {
-    ["<leader>bb"] = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
-    ["<leader>fb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    ["<leader>fc"] = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    ["<leader>f"] = { "<cmd>Telescope find_files<cr>", "Find files" },
-    ["<leader>fp"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
-    ["<leader>ft"] = { "<cmd>Telescope live_grep<cr>", "Find Text" },
-    ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help" },
-    ["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
-    ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
-  }
+  --wk.register { }
 
   require("telescope").setup {
     defaults = { 
@@ -46,7 +36,75 @@ function M.config()
       previewer = true,
     },
     
-    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require("telescope").load_extension, "fzf")
+  }
+end
+
+  -- Telescope live_grep in git root
+  -- Function to find the git root directory based on the current buffer's path
+ local function find_git_root()
+    -- Use the current buffer's path as the starting point for the git search
+    local current_file = vim.api.nvim_buf_get_name(0)
+    local current_dir
+    local cwd = vim.fn.getcwd()
+    -- If the buffer is not associated with a file, return nil
+    if current_file == '' then
+      current_dir = cwd
+    else
+      -- Extract the directory from the current file's path
+      current_dir = vim.fn.fnamemodify(current_file, ':h')
+    end
+
+    -- Find the Git root directory from the current file's path
+    local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+    if vim.v.shell_error ~= 0 then
+      print 'Not a git repository. Searching on current working directory'
+      return cwd
+    end
+    return git_root
+ end
+
+-- Custom live_grep function to search in git root
+local function live_grep_git_root()
+  local git_root = find_git_root()
+  if git_root then
+    require('telescope.builtin').live_grep {
+      search_dirs = { git_root },
+    }
+  end
+end
+
+
+vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
+
+-- Telescope builtin keymaps
+-- See `:help telescope.builtin`
+
+-- Search open buffers
+vim.keymap.set('n', '<leader>b', function()
+  require('telescope.builtin').buffers()
+end, { desc = "Search existing [b]uffers" })
+
+
+-- Search within the file
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Search the file' })
+
+-- Search files
+vim.keymap.set("n", "<leader>f", function()
+  require("telescope.builtin").find_files()
+end, { desc = "Search [f]iles in current directory"})
+
+
+local function telescope_live_grep_open_files()
+  require('telescope.builtin').live_grep {
+    grep_open_files = true,
+    prompt_title = 'Live Grep in Open Files',
   }
 end
 
