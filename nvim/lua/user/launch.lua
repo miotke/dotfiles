3612a9726729e925ac9b91cb7169cc3b5cc1442f
Andrew Miotke <andrew@andrewmiotke.com>
2024-03-22 17:38:02 -0700

    new vim config

--- nvim/lua/user/launch.lua
+++
@@ -1,8 +0,0 @@
--- Global variable 
-LAZY_PLUGIN_SPEC = {}
-
--- Global function named 'spec' which is callable in init.lua
-function spec(item)
-  table.insert(LAZY_PLUGIN_SPEC, { import = item })
-end
-
